754043d022d39ccfea01ea8dcaebfe410ef561e8
sam10003 <sam.porta2005@gmail.com>
2025-05-12 20:02:58 +0200

    Singleton blueprint

---
+++ src/package1/PointTracker_SingletonPattern.java
@@ -0,0 +1,38 @@
+package package1;
+
+// --Sams guide to the singleton pattern in this game
+//      (1) tbh just keep your hands off this code
+//          especially the public static function
+//      (2) to call this function onto your class use -> PointTracker_SingletonPattern anotherSingleton = Point...,.getInstance();
+
+public class PointTracker_SingletonPattern {
+    private int player1_score;
+    private int player2_score;
+
+    private static PointTracker_SingletonPattern instance;
+
+    PointTracker_SingletonPattern() {
+        player1_score = 0;
+        player2_score = 0;
+    }
+
+    public static PointTracker_SingletonPattern getInstance() {
+        if (instance == null) {
+            instance = new PointTracker_SingletonPattern();
+        }
+        return instance;
+    }
+
+    public int getPlayer1_score() {
+        return player1_score;
+    }
+    public void setPlayer1_score(int player1_score) {
+        this.player1_score = player1_score;
+    }
+    public int getPlayer2_score() {
+        return player2_score;
+    }
+    public void setPlayer2_score(int player2_score) {
+        this.player2_score = player2_score;
+    }
+}
