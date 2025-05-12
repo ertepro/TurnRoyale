693131b8528bfcd0e5eab71a3f9b28b9a66806fe
sam10003 <sam.porta2005@gmail.com>
2025-05-12 21:46:14 +0200

    rah

--- src/package1/GameController.java
+++ src/package1/GameController.java
@@ -1,6 +1,7 @@
 package package1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameController {
 
@@ -33,8 +34,7 @@ public class GameController {
         all_cards.add(package1.TroopFactory.createTroop("Stun_golem"));
 
         //here we begin all the game
-        System.out.println("Player 1, choose your deck");
-        System.out.println("pick 7 cards from: ");
+        System.out.println("there are all the cards:");
         for(Troop t : all_cards){
             TroopActions_StateInterface a = new State_active(); // or State_active, etc.
             t.set_state(a);
@@ -44,40 +44,70 @@ public class GameController {
             );
         }
 
-        System.out.println("Player 2, choose your deck");
+        System.out.println("Player 1, choose your deck");
+        System.out.println("pick 7 cards from the available");
+        // Player 1 picks 7 cards
+        System.out.println("\nPlayer 1, choose your deck by picking 7 cards from the available list.");
+        Scanner scan = new Scanner(System.in);
+        Deck deck1 = new Deck();
+        for (int i = 0; i < 7; i++) {
+            boolean validChoice = false;
+            while (!validChoice) {
+                System.out.println("Enter the card name for card " + (i + 1) + " (e.g., Skeletons, Archers, Giant): ");
+                String cardName = scan.nextLine();
+
+                // Check if the card is in the list
+                Troop chosenCard = null;
+                TroopActions_StateInterface a = null;
+                for (Troop t : all_cards) {
+                    a = t.get_state();
+                    if (a.getName(t).equalsIgnoreCase(cardName)) {
+                        chosenCard = t;
+                        break;
+                    }
+                }
+
+                if (chosenCard != null) {
+                    deck1.addCard(chosenCard); // Add the selected card to the deck
+                    System.out.println("You selected: " + a.getName(chosenCard));
+                    validChoice = true; // Valid choice, exit loop
+                } else {
+                    System.out.println("Invalid card name, please choose a valid card.");
+                }
+            }
+        }
+
+        // Display Player 1's deck
+
+        // Player 2 picks 7 cards
+        System.out.println("\nPlayer 2, choose your deck by picking 7 cards from the available list.");
+        Deck deck2 = new Deck();
+        for (int i = 0; i < 7; i++) {
+            boolean validChoice = false;
+            while (!validChoice) {
+                System.out.println("Enter the card name for card " + (i + 1) + " (e.g., Skeletons, Archers, Giant): ");
+                String cardName = scan.nextLine();
+
+                // Check if the card is in the list
+                Troop chosenCard = null;
+                TroopActions_StateInterface a = null;
+                for (Troop t : all_cards) {
+                    a = t.get_state();
+                    if (a.getName(t).equalsIgnoreCase(cardName)) {
+                        chosenCard = t;
+                        break;
+                    }
+                }
+
+                if (chosenCard != null) {
+                    deck2.addCard(chosenCard); // Add the selected card to the deck
+                    System.out.println("You selected: " + a.getName(chosenCard));
+                    validChoice = true; // Valid choice, exit loop
+                } else {
+                    System.out.println("Invalid card name, please choose a valid card.");
+                }
+            }
+        }
+
     }
-    // START
-    // add score
-    // create players
-    // add observer?
-    // add factory
-    // define turns
-
-
-    // MAIN LOOP
-
-    // menu for selecting each player's deck
-
-    // -Mana manager-
-    // Add mana to the players based on reg
-    // Pay the mana after deploying the card
-
-    // -Turns-
-    // Switch turn
-    // Show menu
-    // Peek a card
-    // Call play Card or not if it has no mana (skip turn)
-
-    // -Battle sys-
-    // resolve combat (TroopA,TroopB)
-
-    // Observers to the towers
-    // Update score
-
-
-
-
-    // end turn
-
-
 }
